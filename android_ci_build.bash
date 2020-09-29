3b0cdb917a9c5b90a2d97bf74f01d2e0ce0d6720
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-09-29 20:48:24 +0700

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -6,8 +6,17 @@ cmake_build () {
   mkdir -p build-$ANDROID_ABI
   mkdir -p lib/$ANDROID_ABI
   cd build-$ANDROID_ABI
-  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_PLATFORM=21 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_SDK_ROOT/ndk-bundle/build/cmake/android.toolchain.cmake
-  cmake --build . --config $BUILD_TYPE
+  cmake \
+        -DANDROID_ABI=$ANDROID_ABI \
+        -DANDROID_STL=c++_shared \
+        -DCMAKE_TOOLCHAIN_FILE=~/android-ndk-r21/build/cmake/android.toolchain.cmake \
+        -DOBOE_SOURCE="$OBOE_LOC" \
+        -DALSOFT_REQUIRE_OBOE=ON \
+        -DALSOFT_REQUIRE_OPENSL=ON \
+        -DALSOFT_EMBED_HRTF_DATA=YES \
+        .
+  # cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_PLATFORM=21 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_SDK_ROOT/ndk-bundle/build/cmake/android.toolchain.cmake
+  cmake --build . --clean-first
   cd ..
   cp build-$ANDROID_ABI/libopenal.so lib/$ANDROID_ABI/
 }
