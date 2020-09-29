b8f6b47abd65ad8a348e38b7d86d3dd16920957e
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-09-29 20:55:41 +0700

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -7,11 +7,12 @@ cmake_build () {
   mkdir -p lib/$ANDROID_ABI
   cd build-$ANDROID_ABI
   cmake \
+        -DANDROID_PLATFORM=21 \
         -DANDROID_ABI=$ANDROID_ABI \
         -DANDROID_STL=c++_shared \
         -DCMAKE_TOOLCHAIN_FILE=$ANDROID_SDK_ROOT/ndk-bundle/build/cmake/android.toolchain.cmake \
         -DOBOE_SOURCE="$OBOE_LOC" \
-        -DALSOFT_REQUIRE_OBOE=ON \
+        -DALSOFT_REQUIRE_OBOE=OFF \
         -DALSOFT_REQUIRE_OPENSL=ON \
         -DALSOFT_EMBED_HRTF_DATA=YES \
         ..
