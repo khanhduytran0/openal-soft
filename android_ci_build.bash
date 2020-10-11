a2e2d68b65a4b791c5906c713c538b782302b067
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-11 12:32:40 +0700

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -12,6 +12,17 @@ cmake_build () {
   cp build-$ANDROID_ABI/libopenal.so lib/$ANDROID_ABI/
 }
 
+# Hack Android NDK to get 64-bit libs linked
+libdir=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib
+mv $libdir/arm-linux-androideabi/16 $libdir/arm-linux-androideabi/16z
+mv $libdir/ai686-linux-android/16 $libdir/i686-linux-android/16z
+cp -R $libdir/aarch64-linux-android/21/* $libdir/aarch64-linux-android/16/
+cp -R $libdir/arm-linux-androideabi/21/* $libdir/arm-linux-androideabi/16/
+cp -R $libdir/i686-linux-android/21/* $libdir/i686-linux-android/16/
+cp -R $libdir/x86_64-linux-android/21/* $libdir/x86_64-linux-android/16/
+
+# /aarch64-linux-android/21/
+
 mkdir -p lib
 
 cmake_build arm64-v8a
