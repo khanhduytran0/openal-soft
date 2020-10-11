eb2f0d6e2c13f79a47a39a26475685f6e162c925
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-11 13:16:48 +0700

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
+export BUILD_ANDROID=true
+
 cmake_build () {
   ANDROID_ABI=$1
   mkdir -p build-$ANDROID_ABI
   mkdir -p lib/$ANDROID_ABI
   cd build-$ANDROID_ABI
   cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_PLATFORM=21 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_SDK_ROOT/ndk-bundle/build/cmake/android.toolchain.cmake
-  cmake -v --build --clean-first .
+  cmake --build . --clean-first --verbose
   cd ..
   cp build-$ANDROID_ABI/libopenal.so lib/$ANDROID_ABI/
 }
