3fcaa61288fb443c7967d2511e316a3380c484d1
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-11 12:42:42 +0700

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -21,13 +21,13 @@ cp -R $libdir/arm-linux-androideabi/21 $libdir/arm-linux-androideabi/16
 cp -R $libdir/i686-linux-android/21 $libdir/i686-linux-android/16
 cp -R $libdir/x86_64-linux-android/21 $libdir/x86_64-linux-android/16
 
-libdir=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include
-mv $libdir/arm-linux-androideabi/16 $libdir/arm-linux-androideabi/16z
-mv $libdir/i686-linux-android/16 $libdir/i686-linux-android/16z
-cp -R $libdir/aarch64-linux-android/21 $libdir/aarch64-linux-android/16
-cp -R $libdir/arm-linux-androideabi/21 $libdir/arm-linux-androideabi/16
-cp -R $libdir/i686-linux-android/21 $libdir/i686-linux-android/16
-cp -R $libdir/x86_64-linux-android/21 $libdir/x86_64-linux-android/16
+# libdir=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include
+# mv $libdir/arm-linux-androideabi/16 $libdir/arm-linux-androideabi/16z
+# mv $libdir/i686-linux-android/16 $libdir/i686-linux-android/16z
+# cp -R $libdir/aarch64-linux-android/21 $libdir/aarch64-linux-android/16
+# cp -R $libdir/arm-linux-androideabi/21 $libdir/arm-linux-androideabi/16
+# cp -R $libdir/i686-linux-android/21 $libdir/i686-linux-android/16
+# cp -R $libdir/x86_64-linux-android/21 $libdir/x86_64-linux-android/16
 
 # /aarch64-linux-android/21/
 
