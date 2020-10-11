991ba977998c11ee42ec59eb7efc28f5038d5f1c
khanhduytran0 <khanhduytran032@gmail.com>
2020-10-11 09:10:49 +0700

    Try build for arm64

--- ci_build.bash
+++ ci_build.bash
@@ -4,10 +4,10 @@ set -e
 export OBOE_LOC=~/oboe
 git clone --depth 1 -b 1.3-stable https://github.com/google/oboe "$OBOE_LOC"
 
-ls *
-
 cmake \​
     -DANDROID_STL=c++_shared \​
+    -DANDROID_PLATFORM=21 \
+    -DANDROID_ABI=arm64-v8a \
     -DCMAKE_TOOLCHAIN_FILE="$ANDROID_SDK_ROOT/ndk-bundle/build/cmake/android.toolchain.cmake" \
     -DOBOE_SOURCE="$OBOE_LOC" \
     -DALSOFT_REQUIRE_OBOE=ON \
@@ -15,6 +15,4 @@ cmake \​
     -DALSOFT_EMBED_HRTF_DATA=YES \
     .
 cmake --build . --clean-first
-
-ls *
 
