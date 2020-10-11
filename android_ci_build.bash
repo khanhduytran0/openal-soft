a9df760bb2a8d722516189a9fc83f9f2cb6e9994
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-11 12:54:30 +0700

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -6,20 +6,20 @@ cmake_build () {
   mkdir -p build-$ANDROID_ABI
   mkdir -p lib/$ANDROID_ABI
   cd build-$ANDROID_ABI
-  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_PLATFORM=16 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_SDK_ROOT/ndk-bundle/build/cmake/android.toolchain.cmake
-  cmake --build . --config $BUILD_TYPE
+  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DANDROID_PLATFORM=21 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_SDK_ROOT/ndk-bundle/build/cmake/android.toolchain.cmake
+  cmake -v --build . --config $BUILD_TYPE
   cd ..
   cp build-$ANDROID_ABI/libopenal.so lib/$ANDROID_ABI/
 }
 
 # Hack Android NDK to get 64-bit libs linked
-libdir=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib
-mv $libdir/arm-linux-androideabi/16 $libdir/arm-linux-androideabi/16z
-mv $libdir/i686-linux-android/16 $libdir/i686-linux-android/16z
-cp -R $libdir/aarch64-linux-android/21 $libdir/aarch64-linux-android/16
-cp -R $libdir/arm-linux-androideabi/21 $libdir/arm-linux-androideabi/16
-cp -R $libdir/i686-linux-android/21 $libdir/i686-linux-android/16
-cp -R $libdir/x86_64-linux-android/21 $libdir/x86_64-linux-android/16
+# libdir=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib
+# mv $libdir/arm-linux-androideabi/16 $libdir/arm-linux-androideabi/16z
+# mv $libdir/i686-linux-android/16 $libdir/i686-linux-android/16z
+# cp -R $libdir/aarch64-linux-android/21 $libdir/aarch64-linux-android/16
+# cp -R $libdir/arm-linux-androideabi/21 $libdir/arm-linux-androideabi/16
+# cp -R $libdir/i686-linux-android/21 $libdir/i686-linux-android/16
+# cp -R $libdir/x86_64-linux-android/21 $libdir/x86_64-linux-android/16
 
 # libdir=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include
 # mv $libdir/arm-linux-androideabi/16 $libdir/arm-linux-androideabi/16z
