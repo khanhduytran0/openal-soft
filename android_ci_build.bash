9736bb549979b1fffe7a5f81cfa5f5db0cb3387a
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-11 12:37:40 +0700

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -16,10 +16,10 @@ cmake_build () {
 libdir=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib
 mv $libdir/arm-linux-androideabi/16 $libdir/arm-linux-androideabi/16z
 mv $libdir/i686-linux-android/16 $libdir/i686-linux-android/16z
-cp -R $libdir/aarch64-linux-android/21/* $libdir/aarch64-linux-android/16/
-cp -R $libdir/arm-linux-androideabi/21/* $libdir/arm-linux-androideabi/16/
-cp -R $libdir/i686-linux-android/21/* $libdir/i686-linux-android/16/
-cp -R $libdir/x86_64-linux-android/21/* $libdir/x86_64-linux-android/16/
+cp -R $libdir/aarch64-linux-android/21 $libdir/aarch64-linux-android/16
+cp -R $libdir/arm-linux-androideabi/21 $libdir/arm-linux-androideabi/16
+cp -R $libdir/i686-linux-android/21 $libdir/i686-linux-android/16
+cp -R $libdir/x86_64-linux-android/21 $libdir/x86_64-linux-android/16
 
 # /aarch64-linux-android/21/
 
